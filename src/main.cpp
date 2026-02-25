9ce9a9411c4ee84f4f52ecfd9b0367528bea1508
nader <nnader@std.mans.edu.eg>
2026-02-26 00:12:49 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
-#include  <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    // Flush after every std::cout / std:cerr
+    cout << unitbuf;
+    cerr << unitbuf;
+
     while (true) {
-        cout << std::unitbuf;
-        cerr << std::unitbuf;
         cout << "$ ";
-        string command;
-        cin >> command;
-        if ( command == "exit" ) {
+
+        string input;
+        getline(cin, input);
+
+        if (input == "exit") {
             exit(0);
         }
-        cout << command << ": command not found" << endl;
+
+        if (input.substr(0, 4) == "echo") {
+            if (input.size() > 5)
+                cout << input.substr(5) << endl;
+            else
+                cout << endl;
+        } else {
+            cout << input << ": command not found" << endl;
+        }
     }
 }
