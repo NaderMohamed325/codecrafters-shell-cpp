d9c00f72536923391946675a6f143eb4b5983873
nader <nnader@std.mans.edu.eg>
2026-02-26 00:36:18 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,80 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
 using namespace std;
 
-int main() {
+
+const string BUILTIN_COMMANDS[] = {"exit", "echo", "type"};
+
+
+vector<string> splitString(const string &str, char delimiter) {
+    vector<string> tokens;
+    string token;
+    stringstream ss(str);
+
+    while (getline(ss, token, delimiter)) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
+
+
+void handleExitCommand() {
+    exit(0);
+}
+
+void handleEchoCommand(const vector<string> &args) {
+    if (args.size() > 1) {
+        for (size_t i = 1; i < args.size(); ++i) {
+            cout << args[i];
+            if (i < args.size() - 1) {
+                cout << " ";
+            }
+        }
+    }
+    cout << endl;
+}
+
+void handleTypeCommand(const vector<string> &args) {
+    if (args.size() > 1) {
+        const string &searchedCommand = args[1];
+        cout << searchedCommand << " is a shell builtin" << endl;
+    } else {
+        cout << "Invalid syntax" << endl;
+    }
+}
+
+void handleUnknownCommand(const string &input) {
+    cout << input << ": command not found" << endl;
+}
+
+
+void processCommand(const string &input) {
+    const vector<string> args = splitString(input, ' ');
+
+    if (args.empty()) {
+        return;
+    }
+
+    if (const string &command = args[0]; command == "exit") {
+        handleExitCommand();
+    } else if (command == "echo") {
+        handleEchoCommand(args);
+    } else if (command == "type") {
+        handleTypeCommand(args);
+    } else {
+        handleUnknownCommand(input);
+    }
+}
+
+// Main Loop
+[[noreturn]] int main() {
     cout << unitbuf;
     cerr << unitbuf;
+
 
     while (true) {
         cout << "$ ";
@@ -11,17 +82,7 @@ int main() {
         string input;
         getline(cin, input);
 
-        if (input == "exit") {
-            exit(0);
-        }
-
-        if (input.substr(0, 4) == "echo") {
-            if (input.size() > 5)
-                cout << input.substr(5) << endl;
-            else
-                cout << endl;
-        } else {
-            cout << input << ": command not found" << endl;
-        }
+        processCommand(input);
     }
+
 }
