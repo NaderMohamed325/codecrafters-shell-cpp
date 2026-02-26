fa40943092eeff7804be7161e7d25719e3267609
nader <nnader@std.mans.edu.eg>
2026-02-26 12:07:08 +0200

    clean up headers in handlers.h and fix JSON formatting in config files

--- src/handlers.h
+++ src/handlers.h
@@ -1,18 +1,14 @@
 #ifndef HANDLERS_H
 #define HANDLERS_H
-#include <cstring>
-#include <iostream>
+#include <bits/stdc++.h>
 #include <string>
 #include <vector>
-#include <sstream>
+
 #include <unistd.h>
 #include <sys/wait.h>
-#include <string>
-#include <vector>
-#include <unistd.h>
-#include <iostream>
-#include <string>
+
 std::vector<std::string> splitString(const std::string &str, char delimiter);
+
 void handleExitCommand();
 
 void handleEchoCommand(const std::vector<std::string> &args);
@@ -22,7 +18,9 @@ void handleTypeCommand(const std::vector<std::string> &args);
 void handlePwdCommand();
 
 void executeExternalCommand(const std::vector<std::string> &args);
-void handleCdCommand(const char * path);
+
+void handleCdCommand(const char *path);
+
 void moveToHome();
 
 #endif // HANDLERS_H
